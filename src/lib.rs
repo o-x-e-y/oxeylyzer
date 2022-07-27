8c91484a918d4459cfd9478d14b48bd317fe77d1
Oxey uwu <luc.oerlemans@ru.nl>
2022-07-27 12:31:49 +0200

    don't `use` everything by default

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 #![feature(let_chains)]
 #![feature(exclusive_range_pattern)]
 
-mod language_data;
-mod load_text;
-mod layout;
-mod trigram_patterns;
-mod utility;
-mod weights;
-mod analyze;
-mod generate;
-mod translation;
-mod languages_cfg;
+pub mod language_data;
+pub mod load_text;
+pub mod layout;
+pub mod trigram_patterns;
+pub mod utility;
+pub mod weights;
+pub mod analyze;
+pub mod generate;
+pub mod translation;
+pub mod languages_cfg;
 
-pub use language_data::*;
-pub use load_text::*;
-pub use layout::*;
-pub use trigram_patterns::*;
-pub use utility::*;
-pub use weights::*;
-pub use analyze::*;
-pub use generate::*;
-pub use translation::*;
-pub use languages_cfg::*;
+// pub use language_data::*;
+// pub use load_text::*;
+// pub use layout::*;
+// pub use trigram_patterns::*;
+// pub use utility::*;
+// pub use weights::*;
+// pub use analyze::*;
+// pub use generate::*;
+// pub use translation::*;
+// pub use languages_cfg::*;
