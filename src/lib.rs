d6e446d5876a1bd5d02c6f7594e6c2e16f998284
Oxey <lucoerlemans37@gmail.com>
2022-09-07 09:40:09 +0200

    remove unnecessary commented out code

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,3 @@ pub mod analyze;
 pub mod generate;
 pub mod translation;
 pub mod languages_cfg;
-
-// pub use language_data::*;
-// pub use load_text::*;
-// pub use layout::*;
-// pub use trigram_patterns::*;
-// pub use utility::*;
-// pub use weights::*;
-// pub use analyze::*;
-// pub use generate::*;
-// pub use translation::*;
-// pub use languages_cfg::*;
