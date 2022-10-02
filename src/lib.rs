95e547dccaac26b4143d4795fa593d85bc01f20a
Oxey <lucoerlemans37@gmail.com>
2022-10-02 11:21:24 +0200

    remove analyze.rs mod in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(fs_try_exists)]
 #![feature(exclusive_range_pattern)]
 #![feature(let_chains)]
+#![feature(is_some_with)]
 
 pub mod language_data;
 pub mod load_text;
@@ -8,7 +9,6 @@ pub mod layout;
 pub mod trigram_patterns;
 pub mod utility;
 pub mod weights;
-pub mod analyze;
 pub mod generate;
 pub mod translation;
 pub mod languages_cfg;
