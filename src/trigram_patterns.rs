5ce8707808138a6cdee997678a9c0c6a796ec00a
oxey:yum <lucoerlemans37@gmail.com>
2022-12-14 20:58:51 +0100

    make `trigram_patterns.rs` more readable

--- src/trigram_patterns.rs
+++ src/trigram_patterns.rs
@@ -1,4 +1,4 @@
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum TrigramPattern {
 	Alternate,
 	AlternateSfs,
@@ -14,35 +14,143 @@ pub enum TrigramPattern {
 	Invalid
 }
 
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+enum Hand {
+	Left,
+	Right
+}
+
+use Hand::*;
+
+impl Hand {
+	const fn eq(self, other: Self) -> bool {
+		self as u8 == other as u8
+	}
+}
+
+impl std::ops::Not for Hand {
+	type Output = Self;
+	
+	fn not(self) -> Self::Output {
+    	match self {
+    		Left => Right,
+    		Right => Left
+    	}
+	}
+}
+
+impl From<Finger> for Hand {
+    fn from(value: Finger) -> Self {
+        value.hand()
+    }
+}
+
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+pub enum Finger {
+	LP,
+	LR,
+	LM,
+	LI,
+	RI,
+	RM,
+	RR,
+	RP,
+	LT,
+	RT
+}
+
+use Finger::*;
+
+impl std::fmt::Display for Finger {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    	let to_write = match self {
+    		LP => "left pinky",
+    		LR => "left ring",
+    		LM => "left middle",
+    		LI => "left index",
+    		RI => "right index",
+    		RM => "right middle",
+    		RR => "right ring",
+    		RP => "right pinky",
+    		LT => "left thumb",
+    		RT => "right thumb"
+    	};
+        write!(f, "{}", to_write)
+    }
+}
+
+impl Finger {
+	pub const fn eq(self, other: Self) -> bool {
+		self as u8 == other as u8
+	}
+	
+	pub const fn gt(self, other: Self) -> bool {
+		self as u8 > other as u8
+	}
+	
+	pub const fn lt(self, other: Self) -> bool {
+		(self as u8) < (other as u8)
+	}
+	
+	const fn hand(&self) -> Hand {
+		match self {
+			LP | LR | LM | LI | LT => Left,
+			_ => Right
+		}
+	}
+	
+	pub const fn from_usize(value: usize) -> Self {
+        match value {
+        	0 => LP,
+        	1 => LR,
+        	2 => LM,
+        	3 => LI,
+        	4 => RI,
+        	5 => RM,
+        	6 => RR,
+        	7 => RP,
+        	8 => LT,
+        	9 => RT,
+			_ => unreachable!()
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Trigram {
-	c1: usize, c2: usize, c3: usize,
-	lh1: bool, lh2: bool, lh3: bool
+	f1: Finger,
+	f2: Finger,
+	f3: Finger,
+	h1: Hand,
+	h2: Hand,
+	h3: Hand
 }
 
 impl std::fmt::Display for Trigram {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {}, {})", self.c1, self.c2, self.c3)
+        write!(f, "{}, {}, {}", self.f1, self.f2, self.f3)
     }
 }
 
 impl Trigram {
-	const fn lh(col: usize) -> bool {
-		col < 4
-	}
-
-	const fn new(c1: usize, c2: usize, c3: usize) -> Self {
+	const fn new(f1: Finger, f2: Finger, f3: Finger) -> Self {
 		Trigram {
-			c1, c2, c3, lh1: Self::lh(c1), lh2: Self::lh(c2), lh3: Self::lh(c3)
+			f1, f2, f3, h1: f1.hand(), h2: f2.hand(), h3: f3.hand()
 		}
 	}
 
 	const fn is_alt(&self) -> bool {
-		self.lh1 != self.lh2 && self.lh2 != self.lh3
+		match (self.h1, self.h2, self.h3) {
+			(Left, Right, Left) => true,
+			(Right, Left, Right) => true,
+			_ => false
+		}
 	}
 
 	const fn get_alternate(&self) -> TrigramPattern {
-		if self.c1 == self.c3 {
+		if self.f1.eq(self.f3) {
 			TrigramPattern::AlternateSfs
 		} else {
 			TrigramPattern::Alternate
@@ -50,52 +158,66 @@ impl Trigram {
 	}
 
 	const fn is_roll(&self) -> bool {
-		let r = self.lh1 as usize + self.lh2 as usize + self.lh3 as usize;
-		!self.is_alt() &&
-		(r == 1 || r == 2) &&
-		self.c1 != self.c2 &&
-		self.c2 != self.c3
+		match (self.h1, self.h2, self.h3) {
+			(Left, Left, Right) => true,
+			(Right, Left, Left) => true,
+			(Right, Right, Left) => true,
+			(Left, Right, Right) => true,
+			_ => false
+		}
 	}
 
 	const fn get_roll(&self) -> TrigramPattern {
-		if self.lh1 && self.lh2 && !self.lh3 {
-			Self::particular_roll(self.c2, self.c1)
-		} else if !self.lh1 && self.lh2 && self.lh3 {
-			Self::particular_roll(self.c3, self.c2)
-		} else if !self.lh1 && !self.lh2 && self.lh3 {
-			Self::particular_roll(self.c1, self.c2)
-		} else if self.lh1 && !self.lh2 && !self.lh3 {
-			Self::particular_roll(self.c2, self.c3)
-		} else {
-			TrigramPattern::Other
+		use TrigramPattern::*;
+		match (self.h1, self.h2, self.h3) {
+			(Left, Left, Right) =>
+				if self.f1.gt(self.f2) { Inroll } else { Outroll }
+			(Right, Left, Left) =>
+				if self.f2.gt(self.f3) { Inroll } else { Outroll }
+			(Right, Right, Left) =>
+				if self.f1.gt(self.f2) { Outroll } else { Inroll }
+			(Left, Right, Right) =>
+				if self.f2.gt(self.f3) { Outroll } else { Inroll }
+			_ => TrigramPattern::Other
 		}
-	}
-
-	const fn particular_roll(f1: usize, f2: usize) -> TrigramPattern {
-		if f1 > f2 {
-			return TrigramPattern::Outroll
-		}
-		TrigramPattern::Inroll
 	}
 
 	const fn on_one_hand(&self) -> bool {
-		self.lh1 == self.lh2 && self.lh2 == self.lh3
+		match (self.h1, self.h2, self.h3) {
+			(Left, Left, Left) => true,
+			(Right, Right, Right) => true,
+			_ => false
+		}
 	}
 
 	const fn is_redir(&self) -> bool {
-		(self.c1 < self.c2 && self.c2 > self.c3) || (self.c1 > self.c2 && self.c2 < self.c3)
+		self.on_one_hand() &&
+		if self.h1.eq(Left) {
+			self.f1.lt(self.f2) && self.f3.lt(self.f2)
+		} else {
+			self.f1.gt(self.f2) && self.f3.gt(self.f2)
+		}
 	}
 
 	const fn is_bad_redir(&self) -> bool {
-		!(self.c1 == 3 || self.c2 == 3 || self.c3 == 3 || self.c1 == 4 || self.c2 == 4 || self.c3 == 4)
+		self.is_redir() && match self.f1 {
+			LI | RI => false,
+			_ => true
+		} && match self.f2 {
+			LI | RI => false,
+			_ => true
+		} && match self.f3 {
+			LI | RI => false,
+			_ => true
+		}
 	}
 
 	const fn has_sfb(&self) -> bool {
-		self.c1 == self.c2 || self.c2 == self.c3
+		self.f1.eq(self.f2) || self.f2.eq(self.f3)
 	}
 
 	const fn is_sft(&self) -> bool {
-		self.c1 == self.c2 && self.c2 == self.c3
+		self.f1.eq(self.f2) && self.f2.eq(self.f3)
 	}
 
 	const fn get_one_hand(&self) -> TrigramPattern {
@@ -103,17 +225,14 @@ impl Trigram {
 			TrigramPattern::Sft
 		} else if self.has_sfb() {
 			TrigramPattern::BadSfb
-		}
-		else if self.is_redir() {
+		} else if self.is_redir() {
 			if self.is_bad_redir() {
 				TrigramPattern::BadRedirect
 			} else {
 				TrigramPattern::Redirect
 			}
-		} else if (self.c1 > self.c2 && self.c2 > self.c3) || (self.c1 < self.c2 && self.c2 < self.c3) {
-			TrigramPattern::Onehand
 		} else {
-			TrigramPattern::Other
+			TrigramPattern::Onehand
 		}
 	}
 
@@ -142,7 +261,11 @@ const fn get_trigram_combinations() -> [TrigramPattern; 512] {
 			let mut c1 = 0;
 			while c1 < 8 {
 				let index = c3*64 + c2*8 + c1;
-				let trigram = Trigram::new(c1, c2, c3);
+				let trigram = Trigram::new(
+					Finger::from_usize(c1),
+					Finger::from_usize(c2),
+					Finger::from_usize(c3)
+				);
 				combinations[index] = trigram.get_trigram_pattern();
 				c1 += 1;
 			}
