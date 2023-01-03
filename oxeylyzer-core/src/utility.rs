753ca928d41656a77901777e16033b42cd28b1ca
oxey:yum <lucoerlemans37@gmail.com>
2023-01-03 14:17:53 +0100

    fix `ConvertU8` handling of bigrams

--- oxeylyzer-core/src/utility.rs
+++ oxeylyzer-core/src/utility.rs
@@ -126,8 +126,14 @@ impl ConvertU8 {
 		}
 	}
 
-	pub fn to_bigram_lossy(&self, from: [char; 2]) -> [u8; 2] {
-		[self.to_single_lossy(from[0]), self.to_single_lossy(from[1])]
+	pub fn to_bigram_lossy(&self, from: [char; 2], char_count: usize) -> usize {
+		let c1 = self.to_single_lossy(from[0]) as usize;
+		let c2 = self.to_single_lossy(from[1]) as usize;
+		if c1 < char_count && c2 < char_count {
+			c1 * char_count + c2
+		} else {
+			u8::MAX as usize
+		}
 	}
 
 	pub fn to_trigram_lossy(&self, from: [char; 3]) -> [u8; 3] {
