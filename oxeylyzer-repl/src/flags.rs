610b4c1b1f6cacf9d278228f20f9617ba4dfc216
oxey <lucoerlemans37@gmail.com>
2026-02-03 19:24:40 +0100

    add `list` as alias for `rank`

--- oxeylyzer-repl/src/flags.rs
+++ oxeylyzer-repl/src/flags.rs
@@ -12,7 +12,7 @@ xflags::xflags! {
             required name2: String
         }
         /// Rank all layouts for the currently specified language. A higher score is better.
-        cmd rank {}
+        cmd rank list {}
         /// Generate a number of layouts and displays the best 10. Note: layouts may not be correct after changing language.
         cmd generate gen g {
             required count: usize
