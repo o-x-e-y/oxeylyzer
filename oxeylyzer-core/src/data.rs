f0a05de9dc10f1fd9316ea48ac37f4ac307e7cef
oxey <lucoerlemans37@gmail.com>
2026-03-18 11:53:00 +0100

    don't print entire data file upon serde error

--- oxeylyzer-core/src/data.rs
+++ oxeylyzer-core/src/data.rs
@@ -116,8 +116,8 @@ impl Data {
 #[cfg(not(target_arch = "wasm32"))]
 impl Data {
     pub fn load<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let content = std::fs::read_to_string(&path).path_context(path)?;
-        let data = serde_json::from_str::<Self>(&content).str_context(content)?;
+        let content = std::fs::read_to_string(&path).path_context(&path)?;
+        let data = serde_json::from_str::<Self>(&content).path_context(path)?;
         Ok(data)
     }
 
